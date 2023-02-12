79176f3da5b544f126b9846319fc9d0c4b2d8618
macharia-cod <72864408+macharia-cod@users.noreply.github.com>
2023-02-12 09:58:35 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """initializes the class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
