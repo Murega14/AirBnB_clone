aa58d73dec544895fc54070382fbaa413a762fdd
Bravin Murega <tedmurega@gmail.com>
2023-02-14 12:55:55 +0000

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
-"""Unittest module for the city class"""
+"""Unittest module for the City Class."""
+
 import unittest
 from datetime import datetime
 import time
+from models.city import City
 import re
 import json
-import os
-from models.city import City
-from models import storage
 from models.engine.file_storage import FileStorage
-from models.base_models import BaseModel
+import os
+from models import storage
+from models.base_model import BaseModel
+
 
 class TestCity(unittest.TestCase):
-    """Test cases for the city class"""
+
+    """Test Cases for the City class."""
 
     def setUp(self):
-        """sets up the test methods"""
+        """Sets up test methods."""
         pass
+
     def tearDown(self):
-        """Tears down the test methods"""
+        """Tears down test methods."""
         self.resetStorage()
         pass
+
     def resetStorage(self):
-        """Resets filestorage data"""
+        """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage_file_path):
-            os.remove(FileStorage._FileStorage_file_path)
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """tests instantiation of class City"""
+        """Tests instantiation of City class."""
+
         b = City()
         self.assertEqual(str(type(b)), "<class 'models.city.City'>")
         self.assertIsInstance(b, City)
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Test attributes of the class City"""
+        """Tests the attributes of City class."""
         attributes = storage.attributes()["City"]
         o = City()
-        for key, value in attributes.items():
-            self.assertTrue(hasattr(o, key))
-            self.assertEqual(type(getattr(o, key, None)), value)
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 
 if __name__ == "__main__":
