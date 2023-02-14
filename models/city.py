08227d9e603f1974c4d7838772665a9e38e95174
Bravin Murega <tedmurega@gmail.com>
2023-02-14 12:50:38 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""the class City inherits from BaseModel"""
-from base_model import BaseModel
+"""This module creates a User class"""
+
+from models.base_model import BaseModel
+
 
 class City(BaseModel):
-    """city class"""
+    """Class for managing city objects"""
+
     state_id = ""
     name = ""
