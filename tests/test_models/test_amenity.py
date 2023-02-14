297d4a9e6be22f3ffc27a9e1aa2c95e4aa85519e
Bravin Murega <tedmurega@gmail.com>
2023-02-14 12:55:02 +0000

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
-"""tests for class amenity"""
+"""Unittest module for the Amenity Class."""
+
 import unittest
-import os
-from models.amenity import Amenity
-from models.base_models import BaseModel
-import time
 from datetime import datetime
+import time
+from models.amenity import Amenity
 import re
 import json
-from models import storage
 from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
 
 class TestAmenity(unittest.TestCase):
-    """Test classes for the Amenity class"""
-    def setup(self):
-        """setting up test methods"""
+
+    """Test Cases for the Amenity class."""
+
+    def setUp(self):
+        """Sets up test methods."""
         pass
+
     def tearDown(self):
-        """Tears down test methods"""
+        """Tears down test methods."""
         self.resetStorage()
         pass
+
     def resetStorage(self):
-        """Resets FileStorage data"""
+        """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
         if os.path.isfile(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """Tests instantiation of amenity class"""
+        """Tests instantiation of Amenity class."""
+
         b = Amenity()
         self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
         self.assertIsInstance(b, Amenity)
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of amenity class"""
+        """Tests the attributes of Amenity class."""
         attributes = storage.attributes()["Amenity"]
-        o = Amenity
-        for key, value in attributes.items():
-            self.assertTrue(hasattr(o, key))
-            self.assertEqual(type(getattr(o, key, None)), value)
+        o = Amenity()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 
 if __name__ == "__main__":
