949041fdf76319996d4678e1be02192794c739f5
Bravin Murega <tedmurega@gmail.com>
2023-02-14 12:53:04 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""class User that inherits from BaseModel"""
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """initializes the class user"""
+    """Class for managing user objects"""
+
     email = ""
     password = ""
     first_name = ""
